e80b371ed3932d29e301addc02c455ad5d90873a
NikitaPlombeer <francuzov72@gmail.com>
2015-04-28 18:58:04 +0300

    First commit

---
+++ desktop/src/com/plombeer/games/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.plombeer.games.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.plombeer.games.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
